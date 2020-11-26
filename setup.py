ea423d61691826954ac9c337f467d36954c999ba
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2020-11-26 20:56:06 +0100

    Remove unneeded MANIFEST file. Fix setup.py package discovery (and test excluding) issues.

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="Flexceptions",
-    version="0.1",
+    version="0.1.1",
     description="Python Flexible Exceptions Library",
     author="Alejandro Cabeza Romero",
     author_email="alex@alexcabeza.io",
-    packages=find_packages("flexceptions", exclude=("tests",)),
+    packages=find_packages(exclude=("*tests",)),
     python_requires=">=3.8",
     classifiers=[
         'Intended Audience :: Developers',
