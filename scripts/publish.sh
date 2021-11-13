86c84a9331a1881a7a37dc3493be15b1073ecb71
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2021-11-13 17:03:49 +0100

    Bump to Python 3.10.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,4 +3,4 @@
 SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPOSITORY_ROOT_DIRECTORY=$SCRIPTS_DIRECTORY/..
 
-python3.8 -m twine upload $REPOSITORY_ROOT_DIRECTORY/dist/*
+python -m twine upload $REPOSITORY_ROOT_DIRECTORY/dist/*
